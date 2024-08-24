a65dbee7eb13d4b832eccf24a862bb1fa14f88dd
sangjinKIM93 <larger611@gmail.com>
2024-08-24 15:32:56 +0900

    Initial Commit

---
+++ EyeProtecter/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  EyeProtecter
+//
+//  Created by 김상진 on 8/24/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ EyeProtecter/EyeProtecterApp.swift
@@ -0,0 +1,17 @@
+//
+//  EyeProtecterApp.swift
+//  EyeProtecter
+//
+//  Created by 김상진 on 8/24/24.
+//
+
+import SwiftUI
+
+@main
+struct EyeProtecterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
